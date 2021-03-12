4ab9e1d46b757d4658571fda626930a7d0597a56
xzhpku <34959461+xuzihan990713@users.noreply.github.com>
2021-03-12 19:21:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -253,7 +253,7 @@ if __name__ == '__main__':
         phantomjs_path = os.path.join('phantomjs', 'phantomjs-windows.exe')
 
     #driver = PhantomJS(executable_path=phantomjs_path)
-    driver= PhantomJS(executable_path=sys_path(), service_args=['--ignore-ssl-errors=true','--ssl-protocol=TLSv1'])
+    driver= PhantomJS(executable_path=phantomjs_path, service_args=['--ignore-ssl-errors=true','--ssl-protocol=TLSv1'])
     run(driver, args.username, args.password, args.campus, args.reason,
         args.destination, args.track, args.habitation, args.district,
         args.street)
